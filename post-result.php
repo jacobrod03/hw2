052a61ca28ba00302e30558c4d1f03573f699965
jacobrod03 <157304839+jacobrod03@users.noreply.github.com>
2024-09-15 20:48:47 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -7,12 +7,12 @@ include "view-header.php";
 if (isset($_POST['my-name'])) {
 ?>
   <p>The value sent is:</p>
-  <?php
+<?php
   echo $_POST['my-name'];
 } else {
-  ?>
+?>
   <p>Nothing posted to the page.</p>
-  <?php
+<?php
 }
 include "view-footer.php"
 ?>
